5c74297797ef8040a5a625b8f85ef70c2dc2c93c
lfjin <lfjin@iflytek.com>
2018-07-06 23:30:20 +0800

    删除多余的包

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.administrator.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
