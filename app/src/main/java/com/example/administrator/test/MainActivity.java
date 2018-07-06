53c704240399a2d112c8b1069522b89feecc4ccd
Li Fan <1614591768@qq.com>
2018-07-06 23:48:05 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "开发新代码", Toast.LENGTH_SHORT).show();
-
+ Toast.makeText(this, "同事代码", Toast.LENGTH_SHORT).show();
     }
 }
