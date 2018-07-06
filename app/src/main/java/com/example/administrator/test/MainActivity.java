60b0793da803caa8438cd0252a583e5bb708d893
Li Fan <1614591768@qq.com>
2018-07-06 23:23:34 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -10,18 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TextView TV = (TextView)findViewById(R.id.tv);
-       // 尝试帮助找回功能
-        Toast.makeText(this, "我的功能全部找回来了123456", Toast.LENGTH_SHORT).show();
-        findViewById(R.id.tv);
-         findViewById(R.id.tv);
-        findViewById(R.id.tv);
-        findViewById(R.id.tv);
-                 findViewById(R.id.tv);
-        findViewById(R.id.tv);
-        findViewById(R.id.tv);
-         findViewById(R.id.tv);
-        findViewById(R.id.tv);
-        findViewById(R.id.tv);
+
     }
 }
